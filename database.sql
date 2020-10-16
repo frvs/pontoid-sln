f561469df960e8c8cf2d7e1a8205fd009c1f33b3
frvs <ffrois.lucas@gmail.com>
2020-10-16 16:18:50 -0300

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,21 @@
+CREATE TABLE dbo.School (
+	Id varchar(50) PRIMARY KEY, 
+	Name varchar(50), 
+	InepCode int
+)
+
+CREATE TABLE dbo.Class (
+	Id varchar(50) PRIMARY KEY, 
+	SchoolId varchar(50) FOREIGN KEY REFERENCES dbo.School(Id), 
+	Description varchar(50), 
+	Grade int, 
+	Turn int
+)
+
+CREATE TABLE dbo.Student (
+	Id varchar(50) PRIMARY KEY, 
+	ClassId varchar(50) FOREIGN KEY REFERENCES dbo.Class(Id), 
+	Name varchar(50), 
+	BirthDate datetime, 
+	Cpf varchar(11)
+)
